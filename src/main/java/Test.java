4cfba57fcefd8d2aa3fc4b7d74398b10d8ccd196
ertugralmert <ertugralmert@gmail.com>
2024-01-11 17:27:12 +0300

    test class eklendi denemedir

---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    /*
+    Sonradan oluşturulan class github deneme süreci
+     */
+    public static void main(String[] args) {
+        System.out.println("merhaba git");
+    }
+}
